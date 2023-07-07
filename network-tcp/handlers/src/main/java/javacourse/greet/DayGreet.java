d21b5e0cdff33a969e9577ab1c5bc9e0e1b046dc
CYD <nontoxic256@gmail.com>
2023-07-07 16:51:43 +0300

    Work with sockets. Network

---
+++ network-tcp/handlers/src/main/java/javacourse/greet/DayGreet.java
@@ -0,0 +1,13 @@
+package javacourse.greet;
+
+
+import javacourse.net.Greetable;
+
+public class DayGreet extends Greetable
+{
+
+    @Override
+    public String buildResponse(String userName) {
+        return "Good day, " + userName;
+    }
+}
